2f9aaf424b96561df48f56c1c79403e7ceb99c37
Patrick Aagaard <patr4547@gmail.com>
2016-01-12 13:56:53 +0100

    implementing score sorting

---
+++ src/Score.java
@@ -0,0 +1,21 @@
+
+public class Score {
+
+	private User user;
+	private int value;
+	
+	public Score(User user, int value){
+		this.user = user;
+		this.value = value;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public int getValue() {
+		return value;
+	}
+	
+	
+}
